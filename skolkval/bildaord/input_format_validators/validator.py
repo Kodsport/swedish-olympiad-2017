3222b5f8e554e75fdc240cdafed53e7b0b90a23a
Arash Rouhani <miffoljud@gmail.com>
2016-11-23 23:28:05 +0700

    bildaord: Probably not even compiling solution

--- skolkval/bildaord/input_format_validators/validator.py
+++ skolkval/bildaord/input_format_validators/validator.py
@@ -18,16 +18,39 @@ assert 1 <= N <= 15
 assert K < N
 assert K == len(data) - 1
 
+# for i in range(1, K+1):
+#     line = data[i].split()
+#     form = line[0]
+#     pos = int(line[1])
+#     num_nums = int(line[2])
+#     nums = map(int, line[3:])
+#     assert form in 'ab'
+#     assert 1 <= pos <= N
+#     assert len(nums) == num_nums
+#     assert pos not in set(nums)
+#     assert are_unique(nums)
+
+valid_chars = 'ABCDEFGHIJKLMNOPQ'[0:N]
 for i in range(1, K+1):
-    line = map(int, data[i].split())
-    form = line[0]
-    pos = int(line[1])
-    num_nums = int(line[2])
-    nums = map(int, line[3:])
-    assert form in 'ab'
-    assert 1 <= pos <= N
-    assert len(nums) == num_nums
-    assert pos not in set(nums)
-    assert are_unique(nums)
+    char = line[0]
+    form = line[1]
+    assert char in valid_chars
+
+    if form == '@':
+        # exempel: C@01,05,12
+        nums_commas = line[2:]
+        every_third_char = nums_commas[2:-1:3]
+        assert set(every_third_char) == set(',')
+        nums = map(int, nums_commas.split(','))
+        assert are_unique(nums)
+        for x in nums:
+            assert 0 < x <= N
+    else:
+        # exempel: B:DEF
+        assert form == ':'
+        chars = line[2:]
+        assert are_unique(chars)
+        for c in chars:
+            assert c in valid_chars
 
 sys.exit(42)
