601bab10b928b8d1fd4597b1ca09c54d4a3b6c76
Karl Lundstig <lundsti@kth.se>
2016-11-29 19:30:38 +0100

    wip roksignaler

---
+++ onlinekval/roksignaler/submissions/accepted/karl_ac.py
@@ -0,0 +1,17 @@
+dict = {}
+
+for i in range(26):
+    letter = input()
+    morse = input()
+    dict[morse] = letter
+
+s = input()
+p = input()
+t = input()
+b = input()
+m = input()
+
+n = input()
+code = input()
+
+print(dict)
