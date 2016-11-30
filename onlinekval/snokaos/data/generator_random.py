a42e0f7513e9a93f7e78af6afff86c6b3a811c19
Fredrik Hernqvist <hernqv@kth.se>
2016-11-30 15:12:11 +0100

    Add testdata generation

--- onlinekval/snokaos/data/generator_random.py
+++ onlinekval/snokaos/data/generator_random.py
@@ -1,7 +1,7 @@
 import sys
-from random import randint
+from random import randint, seed
 
-def gen(n=2000, m=100000, s=-1, p=-1):
+def gen(n=100, m=100000, s=-1, p=-1):
 	if s == -1:
 		s = randint(0, n-1)
 	if p == -1:
@@ -26,6 +26,7 @@ def gen(n=2000, m=100000, s=-1, p=-1):
 
 if __name__ == "__main__":
 
-	args = {key:int(value) for key, value in [tuple(x.split("=")) for x in sys.argv[1:]]}
+	args = {key:int(value) for key, value in [x.split("=") for x in sys.argv[1:-1] if x.find("=") != -1]}
+	seed(int(sys.argv[len(sys.argv)-1]))
 
 	gen(**args)
