6d84a554395f01c7b071865023b099f73d2d9886
Arash Rouhani <arashrk@vng.com.vn>
2016-11-30 15:00:22 +0700

    snobollskastning1: First mostly untested solution

--- onlinekval/snobollsskrig1/submissions/accepted/arash_ac.py
+++ onlinekval/snobollsskrig1/submissions/accepted/arash_ac.py
@@ -1,35 +1,57 @@
+"""
+Arash AC(100) solution to snobollskastning1
+"""
 import sys
-import itertools
+import collections
+import heapq
 
-# TODO snobollskrig1
 
-# def solve(lines):
-#     def resolve_device(line):
-#         if line[1] == 'mobile':
-#             return (line[0] + 100, line[2])
-#         else:
-#             return (line[0], line[2])
-#     time_actions = sorted(map(resolve_device, lines))
-#     result = 0
-#     play_time = -1
-#     pause_time = 0
-#     for (time, action) in time_actions:
-#         if play_time > pause_time:
-#             result += time - play_time
-#         if action == 'play':
-#             play_time = time
-#         else:
-#             pause_time = time
-#     return result
+def main():
+    stdin_lines = []
+    for line in sys.stdin:
+        if line.strip() != "":
+            stdin_lines.append(line.strip())
 
-# if __name__ == '__main__':
-#     # N,M = map(int, sys.stdin.readline().split())
-#     N = int(sys.stdin.readline())
-#     raw_lines = sys.stdin.readlines()
-#     def parse_line(line):
-#         segs = line.split(' ')
-#         segs[0] = int(segs[0])
-#         segs[2] = segs[2].rstrip()
-#         return segs
-#     lines = map(parse_line, raw_lines)
-#     print(solve(lines))
+    # parse step 1
+    n, l, _m = map(int, stdin_lines[0].split())
+    # parse step 2
+    start_positions = list(map(int, stdin_lines[1:l+1]))
+    # parse step 3
+    graph = collections.defaultdict(list)
+    Edge = collections.namedtuple('Edge', 'dest weight')
+    for line in stdin_lines[l+1:]:
+        v1, v2, w = map(int, line.split())
+        e1 = Edge(v2, w)
+        e2 = Edge(v1, w)
+        graph[v1].append(e1)
+        graph[v2].append(e2)
+    # Done parsing, start solving
+    answer = solve(start_positions, graph)
+    for (a, b) in answer:
+        print(a, b)
+
+
+def solve(start_positions, graph):
+    answer = set()
+    heap = [(0, node_ix, country_ix) for (country_ix, node_ix) in enumerate(start_positions)]
+    heapq.heapify(heap)
+    owner_map = {}
+    while heap:
+        (t0, v0, c0) = heapq.heappop(heap)
+        if v0 in owner_map:
+            # Not needed as they will have attacked us already
+            # answer.append(set((c0, owner_map[v0])))
+            continue
+        owner_map[v0] = c0
+        for edge in graph[v0]:
+            if edge.dest in owner_map:
+                # It's a conquered node, lets attack unconditionally
+                answer.add(frozenset((c0, owner_map[edge.dest])))
+            else:
+                # It's free to try to reach
+                heapq.heappush(heap, (t0 + edge.weight, edge.dest, c0))
+    return (sorted(x) for x in answer if len(x) > 1)
+
+
+if __name__ == '__main__':
+    main()
