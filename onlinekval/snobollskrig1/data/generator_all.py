741bad33ac5e326268131c81ba4c67b2d4f4c680
Arash Rouhani <arashrk@vng.com.vn>
2016-12-01 18:03:44 +0700

    snobollskrig1: Helt otestead ofärdig generator

--- onlinekval/snobollskrig1/data/generator_all.py
+++ onlinekval/snobollskrig1/data/generator_all.py
@@ -3,19 +3,56 @@
 import sys
 import random
 
-def sysarg(name):
+def cmdlinearg(name):
     for arg in sys.argv:
         if arg.startswith(name + "="):
             return arg.split("=")[1]
-    raise ValueError('no such argument')
+    return None  # This is ok for this task
 
 def main():
     random.seed(int(sys.argv[-1]))
-    n, l, m = (int(sysarg(c)) for c in 'nlm')
-    mode = sysarg('mode')
+    n, l, m = (int(cmdlinearg(c)) for c in 'nlm')
+    assert n
+    assert l
+    mode = cmdlinearg('mode')
+    node_indexes = list(range(n))
+
     if mode == 'linje':
-        assert n == m+1
-    start_positsions = random.sample(list(range(n), l)
+        assert not m
+        m = n-1
+        def edge(i):
+            return (i, i+1)
+        def weight(i):
+            return random.randint(5, 100)
+    elif mode == 'inge_nodkrig':
+        assert not m
+        ISLAND_SIZE = 10
+        m = 2*n
+        def edge(i):
+            if i < n:
+                # within an island
+                j = n - (n%ISLAND_SIZE)
+                return random.sample(range(j, j + ISLAND_SIZE), 2)
+            else:
+                # connecting islands
+                return random.sample(node_indexes, 2)
+        def weight(i):
+            if i < n:
+                # within an island
+                return 1 << (n%ISLAND_SIZE)
+            else:
+                # connecting islands
+                return random.randint(10000000, 20000000)
+        pass
+    elif mode == 'normal':
+        def edge(i):
+            return random.sample(node_indexes, 2)
+        def weight(i):
+            return random.randint(5, 10000000)
+        assert m, "You must set m here"
+    else:
+        assert False, "mode is not valid value"
+    start_positions = random.sample(node_indexes, l)
 
     # TODO
 
