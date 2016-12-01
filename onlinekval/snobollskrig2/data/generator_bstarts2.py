ba231f9be6d6008e5b0776845f05f0f0cd136bf0
Simon Lindholm <simon.lindholm10@gmail.com>
2016-12-01 17:56:31 +0100

    snobollskrig2: fix generator for small to give unique values

---
+++ onlinekval/snobollskrig2/data/generator_bstarts2.py
@@ -0,0 +1,32 @@
+import sys
+import random
+
+n,m,seed = map(int, sys.argv[1:])
+random.seed(seed)
+
+a = []
+b = []
+hasb = 0
+nrem = n
+mrem = m
+assert n <= m
+cur = 0
+while len(a) < n or len(b) < m:
+    cur += 1
+    while True:
+        r = random.randint(2, 3)
+        if r == 2 and hasb >= 1 and len(a) < n:
+            a.append(cur)
+            hasb -= 1
+            break
+        elif r == 3 and len(b) < m:
+            b.append(cur)
+            hasb += 1
+            break
+
+assert len(a) == n
+assert len(b) == m
+
+print("{} {}".format(n,m))
+print(" ".join(map(str, a)))
+print(" ".join(map(str, b)))
