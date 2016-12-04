1c7eadfff2bb9e93422f6848c42678d86989979a
Simon Lindholm <simon.lindholm10@gmail.com>
2016-12-04 19:56:26 +0100

    snomur: fix validator

--- onlinekval/snomur/output_validators/validator.cpp
+++ onlinekval/snomur/output_validators/validator.cpp
@@ -4,6 +4,7 @@ using namespace std;
 #define rep(i, from, to) for (int i = from; i < int(to); ++i)
 #define sz(x) (int)(x).size()
 typedef vector<int> vi;
+typedef pair<int, int> pii;
 
 string input_file, output_dir, answer_file;
 
@@ -37,6 +38,24 @@ bool ok2(const set<pair<int, bool>>& below, int point) {
 bool ok(const set<pair<int, bool>>& below, const set<pair<int, bool>>& above) {
 	for (auto& it : above) {
 		if (!ok2(below, it.first)) return false;
+	}
+	return true;
+}
+
+bool ok3(const vector<pii>& below, const vector<pii>& above) {
+	int prev = -1;
+	const int inf = 1 << 29;
+	for (auto& it : above) {
+		int cur = it.first;
+		if (prev != -1) {
+			auto it = lower_bound(below.begin(), below.end(), pii(prev, inf));
+			auto it2 = lower_bound(below.begin(), below.end(), pii(cur, -1));
+			if (it != it2) return false;
+			assert(it != below.begin());
+			--it;
+			if (it->second < cur) return false;
+		}
+		prev = it.second;
 	}
 	return true;
 }
@@ -75,6 +94,8 @@ int main(int argc, char** argv) {
 	cin >> H;
 	if (H < 0) die("invalid height");
 	set<pair<int, bool>> row;
+	vector<pii> row2;
+	row2.push_back(pii(0, W));
 	row.insert(make_pair(0, true));
 	row.insert(make_pair(W, false));
 	rep(i,0,H) {
@@ -82,6 +103,7 @@ int main(int argc, char** argv) {
 		cin >> B;
 		if (B <= 0) die("invalid number of blocks");
 		set<pair<int, bool>> nrow;
+		vector<pii> nrow2;
 
 		int lastPos = 0;
 		rep(j,0,B) {
@@ -91,10 +113,14 @@ int main(int argc, char** argv) {
 			if (blocks[L]-- == 0) die("try to use block we dont have");
 			int R = P + L;
 			if (R <= 0 || P + L > W) die("invalid right position of block");
-			if (R < lastPos) die("new block is not to the right!");
+			if (P < lastPos) die("new block is not to the right!");
 			lastPos = R;
 			nrow.insert(make_pair(P, true));
 			nrow.insert(make_pair(R, false));
+			if (!nrow2.empty() && nrow2.back().second == P)
+				nrow2.back().second = R;
+			else
+				nrow2.push_back(pii(P, R));
 		}
 		if (lastPos != W) {
 			die("did not cover row edges");
@@ -103,8 +129,10 @@ int main(int argc, char** argv) {
 		// Add infinite fake blocks to left and right
 		row.insert(make_pair(0, false));
 		row.insert(make_pair(W, true));
-		if (!ok(row, nrow)) die("bad hole config");
+		if (!ok(row, nrow)) die("gap over hole");
+		if (!ok3(row2, nrow2)) die("hole over hole");
 		row = nrow;
+		row2 = nrow2;
 	}
 	assert_done(cin);
 	accept(10 * double(H) / double(best));
