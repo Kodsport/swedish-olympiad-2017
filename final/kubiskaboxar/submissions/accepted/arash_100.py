681bc826554d026dbf48b36c1312189b7c4296f1
Arash Rouhani <miffoljud@gmail.com>
2016-12-18 13:18:21 +0700

    kubiskaboxar: 100p solution

--- final/kubiskaboxar/submissions/accepted/arash_100.py
+++ final/kubiskaboxar/submissions/accepted/arash_100.py
@@ -4,8 +4,9 @@ Arash AC(100) solution to kubiska boxar
 """
 #pylint: disable=missing-docstring, invalid-name
 import sys
+import itertools
+import collections
 from pprint import pprint  # pylint: disable=unused-import
-
 
 def main():
     stdin_lines = []
@@ -13,10 +14,47 @@ def main():
         if line.strip() != "":
             stdin_lines.append(line.strip())
 
-    # parse step 1
-    n = int(stdin_lines[0])
+    # Ignore parsing first line
     # parse step 2
-    print("TODO", n)
+
+    Box = collections.namedtuple('Box', 'length color')
+    def parse_line(line):
+        length_str, color = line.split()
+        return Box(int(length_str), color)
+    boxes = [parse_line(line) for line in stdin_lines[1:]]
+    solutions = sorted(solve(boxes))
+    assert len(solutions) == 6
+    assert solutions[0][0] < solutions[1][0]  # Answer is unique
+    solution = solutions[0]
+    score = solution[0]
+    rule = solution[1]
+    print("{} i {}".format(*rule[0:2]))
+    print("{} i {}".format(*rule[1:3]))
+    print(score)
+
+
+def solve(boxes):
+    rules = itertools.permutations('RGB')
+    return ((score_for_rule(boxes, rule), rule) for rule in rules)
+
+def score_for_rule(boxes, rule):
+    smallest = [box for box in boxes if box.color == rule[0]]
+    medium = [box for box in boxes if box.color == rule[1]]
+    biggest = [box for box in boxes if box.color == rule[2]]
+    total_savings = savings(smallest, medium) + savings(medium, biggest)
+    return len(boxes) - total_savings
+
+def savings(smallers, biggers):
+    smallers = sorted(smallers)
+    biggers = sorted(biggers)
+    j = 0
+    for big in biggers:
+        if j >= len(smallers):
+            break
+        small = smallers[j]
+        if big > small:
+            j += 1  # Make the smallest bigger
+    return j
 
 
 if __name__ == '__main__':
