05aa30429b2f6794aad5a3e3773d6719d1b5908c
Simon Lindholm <simon.lindholm10@gmail.com>
2017-02-03 21:36:50 +0100

    snomurskontrollant: update input validator to comply with statement

--- final/snomurskontrollant/input_format_validators/validator.py
+++ final/snomurskontrollant/input_format_validators/validator.py
@@ -7,11 +7,12 @@ W, H = map(int, input().split())
 for i in range(H):
     line = list(map(int, input().split()))
     B = line[0]
-    lastpos = -1e9
+    lastpos = 0
     for j in range(0, B):
         pos = line[2 * j + 1]
         le = line[2 * j + 2]
-        assert pos > lastpos
-        lastpos = pos
+        assert pos >= lastpos
+        lastpos = pos + le
+    assert lastpos <= W
 
 sys.exit(42)
