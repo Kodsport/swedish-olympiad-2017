71c752d5215aadecbe8c71b986f5a36b6a5177e9
Simon Lindholm <simon.lindholm10@gmail.com>
2017-02-03 21:29:44 +0100

    julklappskop: fix group scores

--- final/julklappskop/data/generator.dsl.bash
+++ final/julklappskop/data/generator.dsl.bash
@@ -10,7 +10,7 @@ compile generator_random.py
 samplegroup
 sample 1
 
-group quadratic 0
+group quadratic 8
 tc quad-01 generator_random n=2 k=2 m=5000
 tc quad-02 generator_random n=2 k=2 m=5000
 tc quad-03 generator_random n=2 k=2 m=5000
@@ -18,14 +18,14 @@ tc quad-04 generator_random n=2 k=3 m=5000
 tc quad-05 generator_random n=2 k=10 m=5000
 tc quad-06 generator_random n=2 k=5000 m=5000
 
-group linear 34
+group linear 15
 include_group quadratic
 tc linear-01 generator_random n=2 k=2 m=100000
 tc linear-02 generator_random n=2 k=2 m=100000
 tc linear-03 generator_random n=2 k=10 m=100000
 tc linear-04 generator_random n=2 k=100000 m=100000
 
-group nifty 34
+group nifty 30
 include_group linear
 tc nifty-01 generator_random n=4 k=4 m=100000
 tc nifty-02 generator_random n=8 k=9 m=100000
@@ -33,7 +33,7 @@ tc nifty-03 generator_random n=8 k=30 m=100000
 tc nifty-04 generator_random n=4 k=100000 m=100000
 tc nifty-05 generator_random n=8 k=100000 m=100000
 
-group dp 35
+group dp 27
 tc dp-01 generator_random n=2 k=2 m=200
 tc dp-02 generator_random n=2 k=2 m=200
 tc dp-03 generator_random n=2 k=3 m=200
@@ -42,7 +42,7 @@ tc dp-05 generator_random n=14 k=14 m=200
 tc dp-06 generator_random n=14 k=19 m=200
 tc dp-07 generator_random n=14 k=100 m=200
 
-group large 35
+group large 20
 include_group dp
 include_group nifty
 tc large-01 generator_random n=14 k=14 m=100000
