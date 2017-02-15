60352f913bdef42737aec721c60e74f63e6b5524
Johan Sannemo <johan.sannemo@gmail.com>
2017-02-16 00:55:00 +0100

    pekgalsvalsen: input validators

---
+++ katt1/pekgalsvalsen/input_format_validators/validator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import sys
+
+N, K = map(int, input().split())
+S = input()
+for c in S:
+    assert 0 <= ord(c) - ord('a') < K
+
+for i in range(K):
+    assert chr(ord('a') + i) in S
+
+sys.exit(42)
