a403022780489a5ddfcb9ed29ecae8191404e277
larshum <larshum@kth.se>
2017-02-19 19:50:17 +0100

    poarkivering: add solution when D[i][j] is constant

---
+++ katt1/poarkivering/submissions/accepted/lars.cpp
@@ -0,0 +1,31 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Solution when all numbers D[i][j] (i != j) are the same
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> S(N);
+    for (int i = 0; i < N; i++) {
+        cin >> S[i];        
+    }
+    vector<vector<int>> D(N, vector<int>(N));
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> D[i][j];
+        }
+    }
+
+    int d0 = D[0][1];
+
+    int minSize = *min_element(S.begin(), S.end());
+    int totalSize = 0;
+    for (int i = 0; i < N; i++) {
+        totalSize += min(S[i], d0);
+    }
+    cout << totalSize << '\n';
+}
