c3de13aa1c1c94f01654c821a41946e788e9d9f3
nigus444 <nilsgust@kth.se>
2017-03-02 21:44:35 +0100

    Create gen_random.py

---
+++ katt2/primarfaktor/data/gen_random.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+import sys
+import random
+
+default = {
+    "n": 100,
+    "m": 200,
+    "maxh": 1000000000,
+}
+
+def cmdlinearg(name):
+    for arg in sys.argv:
+        if arg.startswith(name + "="):
+            return arg.split("=")[1]
+    return default[name]
+
+def main():
+    random.seed(int(sys.argv[-1]))
+    n, m = (int(cmdlinearg(name)) for name in ['n', 'm'])
+    maxh = int(cmdlinearg("maxh"))
+    print("{} {}".format(n, m))
+    print(" ".join(str(random.randint(0,maxh)) for x in range(0, n)))
+    S = set()
+    for k in range(0,m):
+        i = random.randint(0,n)
+        j = random.randint(0,n)
+        while not (i != j and (max(i,j)*n+min(i,j)) not in S):
+            i = random.randint(0,n)
+            j = random.randint(0,n)
+        S.add(max(i,j)*n+min(i,j))
+        print(str(i+1) + " " + str(j+1))
+        
+
+if __name__ == "__main__":
+    main()
