586c8b7505cc7c9f841be107caa3a424b403b3f1
EmanuelGedin <egedin@kth.se>
2017-03-03 09:45:14 +0100

    mixed generator

---
+++ katt2/nangijala/data/generator_mixed.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+import sys
+import random
+
+default = {
+    "n": 100000,
+    "m": 100000,
+    "s": 10,
+}
+
+def cmdlinearg(name):
+    for arg in sys.argv:
+        if arg.startswith(name + "="):
+            return arg.split("=")[1]
+    return default[name]
+
+def path(start, end, shuffleNodes):
+    assert end - start > 0
+    for i in range(start, end-1):
+        print("{} {}".format(shuffleNodes[i], shuffleNodes[i+1]))
+
+def cycle(start, end, shuffleNodes):
+    assert end - start > 1
+    for i in range(start, end-1):
+        print("{} {}".format(shuffleNodes[i], shuffleNodes[i+1]))
+    print("{} {}".format(shuffleNodes[end-1], shuffleNodes[start]))
+
+def main():
+    random.seed(int(sys.argv[-1]))
+    n, m, s = (int(cmdlinearg(name)) for name in ['n', 'm', 's'])
+    print("{} {}".format(n, m))
+    shuffleNodes = [i for i in range(0, n)]
+    random.shuffle(shuffleNodes)
+    i = 0
+    isCycle = True
+    while m > 0:
+        if isCycle and m > 2:
+            length = min(s, m)
+            cycle(i, i+s, shuffleNodes)
+            m -= s
+            i += length
+            isCycle = False
+        else:
+            length = min(s, m+1)
+            path(i, i+s, shuffleNodes)
+            m -= s - 1
+            i += length
+            isCycle = True
+
+if __name__ == "__main__":
+    main()
