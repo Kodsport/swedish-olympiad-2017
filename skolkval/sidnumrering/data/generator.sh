0333b33192dc5a193c64de711b64cd885061078d
hairez <harryzhang2004@hotmail.com>
2023-09-16 22:07:41 +0200

    [sidnumrering] fix duplicate testcases

--- skolkval/sidnumrering/data/generator.sh
+++ skolkval/sidnumrering/data/generator.sh
@@ -46,12 +46,12 @@ tc g2-07 gen minN=$((10**6)) maxN=$((10**8))
 tc g2-08 gen minN=$((10**9))
 tc g2-09 gen minN=$((10**10))
 tc g2-10 gen minN=$((10**10))
-tc g2-11 gen n=100000000000
+tc g2-11 gen n=100000000123
 tc g2-12 gen n=100000000001
 tc g2-13 gen n=123456789012
 tc g2-14 gen n=999999999999
 tc g2-15 gen n=999999999998
-tc g2-16 gen n=1000000000000
+tc g2-16 gen n=100000001234
 tc g2-17 gen minN=$((10**11))
 tc g2-18 gen minN=$((10**5)) maxN=$((10**6))
 tc g2-19 gen minN=$((10**6)) maxN=$((10**7))
