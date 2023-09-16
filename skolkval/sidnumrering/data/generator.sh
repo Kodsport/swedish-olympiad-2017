1660d90de7da94009adc675fcbf3918f1c45a123
hairez <harryzhang2004@hotmail.com>
2023-09-16 18:08:40 +0200

    [sidnumrering] update generator.sh

--- skolkval/sidnumrering/data/generator.sh
+++ skolkval/sidnumrering/data/generator.sh
@@ -35,7 +35,6 @@ tc_manual ../manual_testdata/manual2.in
 
 
 group g2 40
-limits maxN=$((10**12)) 
 include_group g1
 tc g2-01 gen minN=$((10**9)) maxA=$((10**10))
 tc g2-02 gen minN=$((10**9)) maxA=$((10**10))
@@ -47,12 +46,12 @@ tc g2-07 gen minN=$((10**6)) maxN=$((10**8))
 tc g2-08 gen minN=$((10**9))
 tc g2-09 gen minN=$((10**10))
 tc g2-10 gen minN=$((10**10))
-tc g2-11 gen minN=$((10**11))
-tc g2-12 gen minN=$((10**11))
-tc g2-13 gen minN=$((10**11))
-tc g2-14 gen minN=$((10**11))
-tc g2-15 gen minN=$((10**11))
-tc g2-16 gen minN=$((10**11))
+tc g2-11 gen n=100000000000
+tc g2-12 gen n=100000000001
+tc g2-13 gen n=123456789012
+tc g2-14 gen n=999999999999
+tc g2-15 gen n=999999999998
+tc g2-16 gen n=1000000000000
 tc g2-17 gen minN=$((10**11))
 tc g2-18 gen minN=$((10**5)) maxN=$((10**6))
 tc g2-19 gen minN=$((10**6)) maxN=$((10**7))
