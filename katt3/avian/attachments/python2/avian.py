a5ea0a8ffd0f3dc1cae7b0c72adc59597bb7e35a
matistjati <matistjati@outlook.com>
2024-05-14 01:59:46 +0200

    rename katt2 to läger, remove katt3 (NOI)

--- katt3/avian/attachments/python2/avian.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python2
-def encode(C, K, N, X):
-    return ['0'*N for i in range(K)]
-
-def decode(C, K, N, Y, I):
-    return '0' * (C*N)
--- katt3/avian/include/cpp/avian.h
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-vector<string> encode(int C, int K, int N, string X);
-
-string decode(int C, int K, int N, vector<string> Y, vector<int> I);
