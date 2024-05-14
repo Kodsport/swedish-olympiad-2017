f6c56e7ef61cae4e06d5db43627f64c3a145d139
hairez <harryzhang2004@hotmail.com>
2024-05-14 17:10:24 +0200

    [bastubad] fix generator.sh

--- final/bastubad/data/generator.sh
+++ final/bastubad/data/generator.sh
@@ -13,40 +13,41 @@ sample 1
 sample 2
 sample 3
 
+# (centers left of x = 0)
 group same-endpoint 21
 tc se-01 generator_random n=10 t=0.2 cap=8000 seed=1
 tc se-02 generator_random n=10 t=0.8 cap=10000 seed=2
 tc se-03 generator_random n=1000 t=0.2 cap=10000 seed=3
 tc se-04 generator_random n=1000 t=0.8 cap=8000 seed=4
 tc_manual ../manual_testdata/linear.in
-# (centers left of x = 0)
 tc_manual ../manual_testdata/outside-convex.in
 tc_manual ../manual_testdata/outside-concave.in
 
+# (half of these are optimized at t = 0, due to the random choice of 'midp')
 group convex 29
 tc 1
-tc 2
-# (half of these are optimized at t = 0, due to the random choice of 'midp')
+tc 3
 tc convex-01 generator_random n=10 seed=1234 t=convex
 tc convex-02 generator_random n=10 seed=1237 t=convex
 tc convex-03 generator_random n=1000 seed=1230 t=convex
 tc convex-04 generator_random n=1000 seed=1231 t=convex
-tc_manual ../manual_testdata/outside-convex.in
+tc outside-convex 
 
-group concave 38
-tc 3
+
 # (the one with seed=1004 is optimized at a non-cutoff point.
 # this is harder to trigger with large n, so we don't bother.)
+group concave 38
+tc 2
 tc concave-01 generator_random n=10 t=concave
 tc concave-02 generator_random n=10 t=concave
 tc concave-03 generator_random n=10 t=concave
 tc concave-04 generator_random n=10 seed=1004 t=concave
 tc concave-05 generator_random n=1000 t=concave
 tc concave-06 generator_random n=1000 t=concave
-tc_manual ../manual_testdata/outside-concave.in
+tc outside-concave
 
 group large 12
-include_group samplegroup
+include_group sample
 include_group same-endpoint
 include_group convex
 include_group concave
