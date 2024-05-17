f66696f2cfec27b0a5483e1eff9896b89b75f453
Abduvohid <ilhomovvahid@gmail.com>
2024-05-17 11:19:25 +0200

    Updated kubiskaboxar

--- final/kubiskaboxar/data/gen.sh
+++ final/kubiskaboxar/data/gen.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-
+REQUIRE_SAMPLE_REUSE=0
 PPATH=$(realpath ..)
 . ../../testdata_tools/gen.sh
 
-setup_dirs
 use_solution arash_100.py
 compile generator_all.py
 
@@ -13,20 +12,17 @@ sample 2
 sample 3
 sample 4
 
-group enforce_rgb 50
+group 1-enforce_rgb 50
 tc g1-1 generator_all n=100  max_l=100000 enforce_rgb=yes
 tc g1-2 generator_all n=200  max_l=200000 enforce_rgb=yes
 tc g1-3 generator_all n=300  max_l=300000 enforce_rgb=yes
 tc g1-4 generator_all n=400  max_l=400000 enforce_rgb=yes
 tc g1-5 generator_all n=1000 max_l=500000 enforce_rgb=yes
 
-group all 50
-include_group enforce_rgb
+group 2-all 50
+include_group 1-enforce_rgb
 tc g2-1 generator_all n=100  max_l=100000
 tc g2-2 generator_all n=200  max_l=200000
 tc g2-3 generator_all n=300  max_l=300000
 tc g2-4 generator_all n=400  max_l=400000
 tc g2-5 generator_all n=1000 max_l=500000
-
-generate_grader
-cleanup_programs
