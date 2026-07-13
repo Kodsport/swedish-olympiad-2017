28100a85bf54a7173211a74f475e32440de8e775
Joshua Andersson <matistjati@outlook.com>
2026-07-13 18:15:51 +0200

    Sync up testdata_tools

--- skolkval/testdata_tools/examples/arithmetic/submissions/partially_accepted/false.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python3
-# @EXPECTED_GRADES@ RTE RTE RTE RTE
-
-assert False
--- skolkval/testdata_tools/examples/ninetynine/data/echo.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# Prints the input, then the random seed (for use by the interactive validator)
-echo "$@"
