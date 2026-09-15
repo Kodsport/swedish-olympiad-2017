e24b3e10019d183ab8911566a399c4949ce502af
Joshua Andersson <matistjati@outlook.com>
2026-09-15 23:08:53 +0200

    [labyrint] fix data score aggregation

--- lager/labyrint/data/generator.sh
+++ lager/labyrint/data/generator.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 REQUIRE_SAMPLE_REUSE=0
-PPATH=$(realpath ..)
 . ../../testdata_tools/gen.sh
 
 use_solution david.cpp
-compile generator_existing.py
-compile generator_random.py
+#compile generator_existing.py
+#compile generator_random.py
 
 samplegroup
 sample sample1
 
-group g1
+group g01 10
 tc_manual ../manual_data/1.in
+
+group g02 10
 tc_manual ../manual_data/2.in
+
+group g03 10
 tc_manual ../manual_data/3.in
+
+group g04 10
 tc_manual ../manual_data/4.in
+
+group g05 10
 tc_manual ../manual_data/5.in
+
+group g06 10
 tc_manual ../manual_data/6.in
+
+group g07 10
 tc_manual ../manual_data/7.in
+
+group g08 10
 tc_manual ../manual_data/8.in
+
+group g09 10
 tc_manual ../manual_data/9.in
+
+group g10 10
 tc_manual ../manual_data/10.in
 #tc tiny-ex generator_existing n=6 m=6
 #tc small generator_existing n=10 m=7 dup=0
